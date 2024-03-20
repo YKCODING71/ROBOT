28ba288b9ff2854b9cfcab63ed2234f55b3a27e4
YK_FILE <62005872+YKCODING71@users.noreply.github.com>
2024-03-20 19:18:01 +0900

    Update ex2.cpp

--- LINUX/ex2.cpp
+++ LINUX/ex2.cpp
@@ -4,10 +4,10 @@ int main(void)
 {
    int test[5] = {80,60,55,22,75};
 
-   printf("test[0]의 값은 %d입니다. \n", test[0]);
-   printf("test[0]의 어드레스는 %p입니다. \n", &test[0]);
-   printf("test[1]의 값은 %d입니다. \n", test[1]);
-   printf("test[1]의 어드레스는 %p입니다. \n", &test[1]);
+   printf("test[0] data =  %d. \n", test[0]);
+   printf("test[0] address = %p. \n", &test[0]);
+   printf("test[1] data =  %d. \n", test[1]);
+   printf("test[1] address = %p. \n", &test[1]);
 
    return 0;
 }
